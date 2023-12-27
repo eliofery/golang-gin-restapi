d0fe72957d520f6eb37d62da1e3180d5968e4ec2
Sergey Erofeev <erofsey@yandex.kz>
2023-12-27 17:58:21 +0600

    Добавил таблицу пользователей

--- database/database.go
+++ database/database.go
@@ -21,6 +21,17 @@ func Init() {
 }
 
 func createTables() {
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)`
+	_, err := DB.Exec(createUsersTable)
+	if err != nil {
+		panic("Не удалось создать таблицу пользователей: " + err.Error())
+	}
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,10 +39,10 @@ func createTables() {
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER
+		user_id INTEGER,
+		FOREIGN KEY (user_id) REFERENCES users(id)
 	)`
-
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 	if err != nil {
 		panic("Не удалось создать таблицу событий: " + err.Error())
 	}
